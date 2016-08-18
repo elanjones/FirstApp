69f96d8864ae62b19a611b45d82c3440e17645e1
Élan Jones <elan.jones@gmail.com>
2016-08-17 20:16:48 -0500

    Set up registration form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controler?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit({rolls: []},:email, :password, :password_confirmation, :name, :username, :bio, :location)}
+
+    devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:email, :password, :password_confirmation, :current_password, :name, :Username, :bio, :location)}
+  end
+
+
+
+
+
 end
