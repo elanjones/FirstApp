ef62599e18d2944f52c2498400e49789fd91aff2
Élan Jones <elan.jones@gmail.com>
2016-08-22 20:46:04 -0500

    Added validations for @username and tweetz

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,6 @@
 class Tweet < ApplicationRecord
   belongs_to :user
 
+  validates :message, presence: true, length: {maximum: 140, too_long: 'A tweet is 140 characters max.  Everybody knows that!'}
+
 end
